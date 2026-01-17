4eae51e9bd68095663751d11c8b0867430630386
Paweł Jarosz <jarosz.pawel@outlook.com>
2026-01-17 11:29:46 +0100

    Initialize app independent library

---
+++ marie-toolbox/src/instruction_set.rs
@@ -0,0 +1,16 @@
+enum InstructionSet {
+    Load,
+    Store,
+    Add,
+    Subt,
+    Input,
+    Output,
+    Halt,
+    Skipcond,
+    Jump,
+    JnS,
+    Clear,
+    AddI,
+    LoadI,
+    StoreI
+}
