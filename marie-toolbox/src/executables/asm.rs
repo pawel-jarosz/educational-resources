1fe84e3014f0a8870eec0a298ad12ea033e2b592
Paweł Jarosz <jarosz.pawel@outlook.com>
2026-01-19 12:48:59 +0100

    Add skeleton of the assembler application

--- marie-toolbox/src/executables/asm.rs
+++ marie-toolbox/src/executables/asm.rs
@@ -1,5 +1,31 @@
+use std::io::BufReader;
+use std::io::BufRead;
+use std::fs::File;
+use std::path::Path;
+
 use super::Context;
 
-pub fn asm_main(context: Context) {
+fn assembly(input: &File, output_filename: &String) {
+    let mut buffer_reader = BufReader::new(input);
+    for line in buffer_reader.lines() {
+        let unpacked = line.unwrap();
+        println!("{}", unpacked); 
+    }  
+}
 
+pub fn asm_main(context: Context) {
+    // availability of this keys is checked already by clap CLI parser
+    let input_filename = context.command_strings.get("input_file").unwrap();
+    let output_filename = context.command_strings.get("output_file").unwrap();
+
+    info!("Generate assembly code for {}", input_filename);
+    info!("Result will be in the {} file", output_filename);
+
+    let input_file = File::open(input_filename);
+    if let Ok(input) = &input_file {
+        assembly(input, output_filename);
+    }
+    else {
+        error!("{}", input_file.err().unwrap());
+    }
 }
